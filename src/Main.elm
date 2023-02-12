fd1cf9dd5101a7e3d46d11908cd82b55a45754bc
idasilva1 <iuribrindeiro@gmail.com>
2023-02-12 12:12:41 -0300

    Adding and removing items from cart

--- src/Main.elm
+++ src/Main.elm
@@ -1,52 +1,123 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
-import Html exposing (input)
-import Html.Attributes exposing (value)
-import Html.Attributes exposing (placeholder)
-
-import Html.Events exposing (onInput)
+import Html exposing (Html, br, button, div, li, ol, text)
 import Html.Attributes exposing (style)
+import Html.Events exposing (onClick)
 
 
-type alias Model = { name: String, count: Int }
-main : Program () Model Msg
+type alias CartLineItemId =
+    String
+
+
+type alias CartLineItem =
+    { cartLineItemId : CartLineItemId, description : String }
+
+
+type alias ShoppingCartModel =
+    { cartId : String, cartLineItems : List CartLineItem }
+
+
+type alias Model =
+    { shoppingCart : ShoppingCartModel, selectedItems : List CartLineItemId }
+
+
+main : Program () Model ShoppingCartMessage
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
 
 
 init : Model
-init = { name = "", count = 0}
+init =
+    { shoppingCart = { cartId = "123", cartLineItems = [ { cartLineItemId = "1", description = "Laptop" } ] }, selectedItems = [] }
 
-type Msg = Increment | Decrement | SetName String
 
-update : Msg -> Model -> Model
-update msg model =
-  case msg of
-    Increment ->
-      { model | count = model.count + 1 } 
+type ShoppingCartMessage
+    = AddItem CartLineItem
+    | RemoveItem CartLineItemId
+    | TogglItemSelection CartLineItemId
+    | ClearSelectedItems
+    | ClearCart
 
-    Decrement ->
-      { model | count = model.count - 1 }
 
-    SetName name -> { model | name = name }
+update : ShoppingCartMessage -> Model -> Model
+update msg ({ shoppingCart, selectedItems } as model) =
+    case msg of
+        AddItem newCartItem ->
+            { model | shoppingCart = { shoppingCart | cartLineItems = newCartItem :: shoppingCart.cartLineItems } }
 
-view : Model -> Html Msg
+        RemoveItem itemId ->
+            { model | shoppingCart = { shoppingCart | cartLineItems = List.filter (\item -> item.cartLineItemId /= itemId) shoppingCart.cartLineItems } }
+
+        ClearCart ->
+            { model | shoppingCart = { shoppingCart | cartLineItems = [] } }
+
+        TogglItemSelection itemId ->
+            { model
+                | selectedItems =
+                    if List.member itemId selectedItems then
+                        List.filter (\id -> id /= itemId) selectedItems
+
+                    else
+                        itemId :: selectedItems
+            }
+
+        ClearSelectedItems ->
+            { model | selectedItems = [] }
+
+
+styledContainer : List (Html.Attribute msg) -> List (Html msg) -> Html msg
+styledContainer htmlAttributes children =
+    div
+        ([ style "max-width" "400px"
+         , style "display" "flex"
+         , style "flex-direction" "column"
+         , style "align-items" "center"
+         , style "border" "1px solid black"
+         , style "border-radius" "5px"
+         , style "box-shadow" "0 0 10px 0 rgba(0,0,0,0.5)"
+         , style "margin" "0 auto"
+         ]
+            ++ htmlAttributes
+        )
+        children
+
+
+styledItem : CartLineItem -> List CartLineItemId -> Html ShoppingCartMessage
+styledItem cartItem selectedItems =
+    li
+        [ style "text-decoration"
+            (if List.member cartItem.cartLineItemId selectedItems then
+                "line-through"
+
+             else
+                "none"
+            )
+        , style "cursor" "pointer"
+        , onClick (TogglItemSelection cartItem.cartLineItemId)
+        ]
+        [ text ("Id: " ++ cartItem.cartLineItemId)
+        , br [] []
+        , text ("Description: " ++ cartItem.description)
+        ]
+
+
+view : Model -> Html ShoppingCartMessage
 view model =
-  div [ 
-        style "max-width" "400px"
-      , style "display" "flex"
-      , style "flex-direction" "column"
-      , style "align-items" "center"
-      , style "border" "1px solid black"
-      , style "border-radius" "5px"
-      , style "box-shadow" "0 0 10px 0 rgba(0,0,0,0.5)"
-      ]
-    [ text model.name  
-    , button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model.count) ]
-    , button [ onClick Increment ] [ text "+" ]
-    , input [ placeholder "Enter your name", value model.name, onInput SetName ] []
-    ]
+    div []
+        [ styledContainer []
+            [ text ("CartId: " ++ model.shoppingCart.cartId)
+            , ol []
+                (List.map
+                    (\item ->
+                        styledItem item model.selectedItems
+                    )
+                    model.shoppingCart.cartLineItems
+                )
+            ]
+        , styledContainer [ style "margin-top" "20px" ]
+            [ button [ onClick (AddItem { cartLineItemId = "2", description = "Pen" }) ] [ text "Add Item" ]
+            , button [ onClick (RemoveItem "1") ] [ text "Remove Cart Item" ]
+            , button [ onClick ClearCart ] [ text "Clear Cart" ]
+            ]
+        ]
